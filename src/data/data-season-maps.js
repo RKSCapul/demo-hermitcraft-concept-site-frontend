448a4963e4718fe4a9735b58090ee0bd482d232a
Ryle Capul <kencap119@gmail.com>
2020-08-05 11:06:45 +0800

    season maps fix

---
+++ src/data/data-season-maps.js
@@ -0,0 +1,52 @@
+const hcMaps = [
+  {
+    index: 1,
+    icon: {
+      caption: 'S1',
+      color: 'black',
+    },
+    name: 'Vanila Season 1'
+  },
+  {
+    index: 2,
+    icon: {
+      caption: 'S2',
+      color: 'black',
+    },
+    name: 'Vanila Season 2'
+  },
+  {
+    index: 3,
+    icon: {
+      caption: 'S3',
+      color: 'blue-grey-10',
+    },
+    name: 'Vanila Season 3'
+  },
+  {
+    index: 4,
+    icon: {
+      caption: 'S4',
+      color: 'deep-orange-10',
+    },
+    name: 'Vanila Season 4'
+  },
+  {
+    index: 5,
+    icon: {
+      caption: 'S5',
+      color: 'green-7',
+    },
+    name: 'Vanila Season 5'
+  },
+  {
+    index: 6,
+    icon: {
+      caption: 'S6',
+      color: 'cyan-3',
+    },
+    name: 'Vanila Season 6'
+  }
+]
+
+export default hcMaps;
