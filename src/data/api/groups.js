0accd47eb4a37be74809746f51b87df5e8947380
Ryle Capul <kencap119@gmail.com>
2020-10-11 01:39:09 +0800

    endpoint-refactor - initial commit

---
+++ src/data/api/groups.js
@@ -0,0 +1,66 @@
+/* Requirements */
+var axios = require('axios');
+
+/* Variables */
+const WEB_API = process.env.VUE_APP_ENDPOINT_URL
+
+var hermitGroups;
+var groupList;
+var groupMembers;
+
+/* Get hermit's associated groups */
+function setHermitGroup(data) {
+  hermitGroups = data;
+}
+
+function getHermitGroup() {
+  return hermitGroups;
+}
+
+async function fetchHermitGroup(channel) {
+  await axios
+    .get(`${WEB_API}/api/hermit/user/${channel}/groups`)
+    .then(json => setHermitGroup(json))
+}
+
+/* Get all groups */
+function setGroupList(data) {
+  groupList = data;
+}
+
+function getGroupList() {
+  return groupList;
+}
+
+async function fetchGroupList() {
+  await axios
+    .get(`${WEB_API}/api/group/all`)
+    .then(json => setGroupList(json))
+}
+
+/* Get group and its members */
+function setGroupMembers(data) {
+  groupMembers = data;
+}
+
+function getGroupMembers() {
+  return groupMembers;
+}
+
+async function fetchGroupMembers(name) {
+  await axios
+    .get(`${WEB_API}/api/group/${name}`)
+    .then(json => setGroupMembers(json))
+}
+
+
+
+export {
+  fetchHermitGroup,
+  fetchGroupList,
+  fetchGroupMembers,
+
+  getHermitGroup,
+  getGroupList,
+  getGroupMembers,
+};
